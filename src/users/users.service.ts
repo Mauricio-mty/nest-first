b48168f6d7aea0ff8aa2804b523dbacca0be276c
Mauricio-mty <00000422@uca.edu.sv>
2026-02-18 11:04:51 -0600

    service with typeorm for user :zap:

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,7 +1,11 @@
+import { Repository } from 'typeorm';
 import { Injectable } from '@nestjs/common';
 import {CreateUserDto} from './dto/create-user.dto';
 import { IdService } from '../utils/id/id.service';
 import { LoggerService } from '../logger/logger.service';
+import {User} from '../entities/users.entity';
+import {UserInterface} from './interfaces/user.interface';
+import {InjectRepository} from '@nestjs/typeorm';
 
 @Injectable()
 export class UsersService {
@@ -10,32 +14,25 @@ export class UsersService {
         //agregando los providers
         private readonly idService:IdService,
         private readonly logger:LoggerService,
+        //inyeccion del repositorio de la entidad user
+        @InjectRepository(User)
+        private userRepository:Repository<User>
     ){} 
+    
 
-
-    private users=[
-        {id:'1',name:'Ana'},
-        {id:'2',name:'Paola'},
-    ];
-
-    findAll(){
-        //implementacion de modulo dinamico
-        this.logger.log(`Listando ${this.users.length} usuarios`);
-        return this.users;
+    findAll():Promise<User[]>{
+        return this.userRepository.find();
     }
 
     findOne(id:string){
-        return this.users.find((data)=>data.id==id);
+        return this.userRepository.findOneBy({id:id});
     }
 
     //se implementa undto para el formato 
-    create(dto:CreateUserDto){
-        const newUser = {id:this.idService.generate(),...dto};
-        this.users.push(newUser);
-         
-        //implementacion de modulo dinamico
-         this.logger.log(`Creando ususario ${newUser.id}`);
-
-        return newUser;
+    create(dto:CreateUserDto):Promise<User>{
+    //Create instance a partir del dto
+     const user = this.userRepository.create(dto);
+     //save instance in db
+     return this.userRepository.save(user);
     }
 }
